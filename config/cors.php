d964fc0cf5c9f7ba061fb8960ca2d527b73e138e
Yassine Benhamzah <yassine.bhmz@hotmail.com>
2026-04-13 15:59:46 +0100

    Add CORS configuration for API access

---
+++ config/cors.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://carental.yassinebenhamzah.com',
+        'https://renta-car-frontend-2y3j.vercel.app',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
